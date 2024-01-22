177352f88c709e867804cb53d960991c5a3fd866
bargold100 <goldkbar@post.bgu.ac.il>
2024-01-22 13:04:35 +0200

    Add project files.

---
+++ ConsoleApplicationHangMan.cpp
@@ -0,0 +1,11 @@
+// ConsoleApplicationHangMan.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    std::cout << "Hello World!\n";
+    cout << "Bar's first cpp programing\n";
+}
